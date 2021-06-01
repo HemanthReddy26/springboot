a5091ee27a5a2e1af87da96c04849a6f255aaf45
Hemanth Reddy A <69100062+HemanthReddy26@users.noreply.github.com>
2021-06-01 17:08:54 +0530

    Update Application.java

--- src/main/java/com/hcl/Application.java
+++ src/main/java/com/hcl/Application.java
@@ -9,13 +9,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
-@EnableSwagger2
 public class Application extends SpringBootServletInitializer {
 
 
@@ -27,12 +23,5 @@ public class Application extends SpringBootServletInitializer {
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
          return application.sources(Application.class);
     }
-
-	
-	@Bean
-	   public Docket productApi() {
-	      return new Docket(DocumentationType.SWAGGER_2).select()
-	         .apis(RequestHandlerSelectors.basePackage("com.hcl.controller")).build();
-	   }
 
 }
